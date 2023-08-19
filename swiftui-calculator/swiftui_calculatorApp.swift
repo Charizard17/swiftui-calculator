07e8ffca72355b91db7a7d19ae53cc822a82cef4
Charizard17 <3723esad@gmail.com>
2023-08-19 23:18:35 +0200

    Initial Commit

---
+++ swiftui-calculator/swiftui_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_calculatorApp.swift
+//  swiftui-calculator
+//
+//  Created by Esad Dursun on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
